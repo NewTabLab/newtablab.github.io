7c1c0c1a65010ff53b50f22e8f91244c398c5632
tarikkavaz <tarikkavaz@gmail.com>
2020-03-02 09:51:23 +0300

    yt

--- newtab/links.js
+++ newtab/links.js
@@ -61,9 +61,9 @@ var LINKS = [
         urls: [
             {url: "https://twitter.com/", name: "Twitter", cssstyle: "hp_twitter"},
             {url: "https://www.reddit.com/", name: "reddit", cssstyle: "hp_reddit"},
+            {url: "http://youtube.com/", name: "YouTube", cssstyle: "hp_youtube"},
             {url: "https://alldebrid.com/torrent/", name: "Alldebrid", cssstyle: "hp_putio"},
             {url: "https://app.plex.tv/desktop", name: "Plex", cssstyle: "hp_plex"},
-            {url: "http://youtube.com/", name: "YouTube", cssstyle: "hp_youtube"},
             {url: "https://rarbgmirror.org/torrents.php?category=1;14;15;16;17;21;22;42;18;19;41;27;28;29;30;31;32;40;23;24;25;26;33;34;43;44;45;46;47;48", name: "RARBG", cssstyle: "hp_rarbg"},
             {url: "https://yts.mx/browse-movies", name: "YTS", cssstyle: "hp_yts"},
             {url: "https://thepiratebay.org/", name: "PirateBay", cssstyle: "hp_piratebay"},
