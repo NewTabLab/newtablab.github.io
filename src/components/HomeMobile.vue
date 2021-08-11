9ddcfa2e979d9f0eb86140fec9cc32c2edb4dcd3
Tarik Kavaz <tarikkavaz@gmail.com>
2021-08-11 13:44:48 +0300

    init

---
+++ src/components/HomeMobile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative z-40 w-full col-span-2 font-titillium">
+    <div class="mt-20"><HelloWorld /></div>
+    <div class="mt-20"><Expertise /></div>
+    <div class="mt-20"><DesignAndCoding /></div>
+    <div class="mt-20"><Clients /></div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./content/HelloWorld.vue";
+import Expertise from "./content/Expertise.vue";
+import DesignAndCoding from "./content/DesignAndCoding.vue";
+import Clients from "./content/Clients.vue";
+
+export default {
+  components: {
+    HelloWorld,
+    Expertise,
+    DesignAndCoding,
+    Clients,
+  },
+};
+</script>
