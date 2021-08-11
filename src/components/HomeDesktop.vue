6246c091ad69a6f6060e6d100dcf48e3bc033c16
Tarik Kavaz <tarikkavaz@gmail.com>
2021-08-11 14:13:21 +0300

    clean

---
+++ src/components/HomeDesktop.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="relative w-full col-span-2 font-titillium">
+    <div
+      class="z-50 box"
+      data-0="opacity: 1;"
+      data-100=""
+      data-250=""
+      data-500="opacity: 0;"
+    >
+      <HelloWorld />
+    </div>
+
+    <div
+      class="box"
+      data-0="opacity: 0;"
+      data-500="opacity: 0;"
+      data-1500="opacity: 1;"
+      data-2000="opacity: 0;"
+    >
+      <Expertise />
+    </div>
+    <div
+      class="box"
+      data-0="opacity: 0;"
+      data-2000="opacity: 0;"
+      data-2500="opacity: 1;"
+      data-3000="opacity: 0;"
+    >
+      <DesignAndCoding />
+    </div>
+    <div
+      class="box"
+      data-0="opacity: 0;"
+      data-3000="opacity: 0;"
+      data-3500="opacity: 1;"
+    >
+      <Clients />
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./content/HelloWorld.vue";
+import Expertise from "./content/Expertise.vue";
+import DesignAndCoding from "./content/DesignAndCoding.vue";
+import Clients from "./content/Clients.vue";
+
+export default {
+  components: {
+    HelloWorld,
+    Expertise,
+    DesignAndCoding,
+    Clients,
+  },
+};
+</script>
---
+++ src/components/content/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <p class="mb-4 text-3xl font-bold">Hello World,</p>
+  <p>
+    I'm a Designer & Coder
+    <span class="text-base">, Movie & Music Lover</span>
+  </p>
+  <p>I Design Logos, Corporate Identities, Websites & Apps</p>
+  <p>
+    I'm responsible for managing of over 200 websites run by
+    <a
+      href="https://www.bilgi.edu.tr"
+      class="px-1 text-lg text-white rounded hover:text-gray-200 bg-dotgreen"
+      >Istanbul Bilgi University</a
+    >
+    since 2002.
+  </p>
+  <p>
+    I'm also the founder of
+    <a
+      href="https://newtablab.com"
+      class="px-1 text-lg text-white rounded hover:text-gray-200 bg-dotgreen"
+      >NewTabLab</a
+    >, a Design & Development company based in Istanbul.
+  </p>
+  <p class="mt-2 font-bold">Scroll / Swipe down for more.</p>
+</template>
